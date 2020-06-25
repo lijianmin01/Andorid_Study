9aad36f07c6c61580fac6ea097fc6222556d1cd7
lijianmin01 <lijianmin01@126.com>
2020-06-25 15:18:27 +0800

    0625

--- workSpace/Demo1/myapplication1/src/main/java/com/example/myapplication1/MainActivity.java
+++ workSpace/Demo1/myapplication1/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,6 +1,6 @@
-package com.example.helloworld;
+package com.example.myapplication1;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//显示XML文件布局内肉
+        setContentView(R.layout.activity_main);
     }
 }
