b867513d66dd460e149f295a68acf7e83842dccd
“lijianmin01” <lijianmin01@126.com>
2021-03-20 17:35:32 +0800

    TextView

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*获取控件,通过id获取*/
+//        TextView tv_one = findViewById(R.id.tv_one);
+//        tv_one.setText("Li~");
     }
+
 }
